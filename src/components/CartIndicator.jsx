8ed14e509fb6c1cbee9d548b323ca968a112147f
annoinspace <aneesahalmask@gmail.com>
2022-12-01 11:50:32 +0000

    cart length changes in display

--- src/components/CartIndicator.jsx
+++ src/components/CartIndicator.jsx
@@ -1,15 +1,17 @@
-import { Button } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
-import { FaShoppingCart } from 'react-icons/fa'
+import { Button } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+import { FaShoppingCart } from "react-icons/fa"
+import { useSelector } from "react-redux"
 
 const CartIndicator = () => {
   const navigate = useNavigate()
+  const cartLength = useSelector((state) => state.cart.content.length)
 
   return (
     <div className="ml-auto mt-3 mb-4">
-      <Button color="primary" onClick={() => navigate('/cart')}>
+      <Button color="primary" onClick={() => navigate("/cart")}>
         <FaShoppingCart />
-        <span className="ml-2">0</span>
+        <span className="ml-2">{cartLength}</span>
       </Button>
     </div>
   )
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -7,6 +7,15 @@ const initialState = {
 const mainReducer = (state = initialState, action) => {
   switch (action.type) {
     //multiple cases will happen here, but we'll start with the default
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          content: [...state.cart.content, action.payload]
+        }
+      }
+
     default:
       return state
     // in the case of an unknown action.type, return the current state
